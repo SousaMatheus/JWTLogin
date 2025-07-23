b1e382e5f71a6cbb8dfb0db01191bb1901fb3148
Matheus Sousa <ma.sousa.soares@gmail.com>
2025-07-23 16:08:40 -0300

    Adds `Verification` valueObject

---
+++ JWTLogin.Core/AccountContext/ValueObjects/Verification.cs
@@ -0,0 +1,29 @@
+﻿using JWTLogin.Core.SharedContext.ValueObjects;
+
+namespace JWTLogin.Core.AccountContext.ValueObjects
+{
+    public class Verification : ValueObject
+    {
+        public string Code { get; } = Guid.NewGuid().ToString("N")[..6].ToUpperInvariant();
+        public DateTime? ExpiresAt { get; private set; } = DateTime.UtcNow.AddMinutes(5);
+        public DateTime? VerifiedAt { get; private set} = null;
+        public bool IsActive => VerifiedAt != null && ExpiresAt == null;
+
+        public void Verify(string code)
+        {
+            if(IsActive)
+                throw new InvalidOperationException("Verification is already active.");
+
+            if(ExpiresAt < DateTime.UtcNow)
+                throw new InvalidOperationException("Verification code has expired.");
+
+            if(!string.Equals(code.Trim(), Code.Trim(), StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new ArgumentException("Invalid verification code.", nameof(code));
+            }
+
+            VerifiedAt = DateTime.UtcNow;
+            ExpiresAt = null;
+        }
+    }
+}
